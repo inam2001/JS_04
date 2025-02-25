6e4e92fc2756624ff86d59203572ad31c6632a6d
adnan-qnkhan <adnan.shafi91@gmail.com>
2025-02-25 16:50:43 +0500

    AfterSolution

---
+++ app.js
@@ -0,0 +1,91 @@
+// Chap 16 Q01
+// var mStudents = [""];
+// mStudents[0]="A";
+// mStudents[1]="B";
+// mStudents[2]="C";
+// mStudents[3]="D";
+
+// console.log(mStudents[0], mStudents[1]);
+
+// Chap 16 Q03
+// var mpets = ["Rabbit", "Parrot"];
+
+// Chap 16 Q04
+//var mSNO = [1, 2, 3, 4, 5];
+
+// Chap 16 Q05
+// var mYesNo = [true, false];
+// console.log(typeof(mYesNo[0]));
+
+// Chap 16 Q06
+//var mAr = [1, 2, "Senior", "Junior"];
+
+// Chap 16 Q07
+// var mQual = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+// document.write("QUALIFICATIONS</br></br>");
+
+// for (var i=0; i<mQual.length; i++)
+// {
+//     document.write(i+1 + ") " + mQual[i] + "</br>");
+// }
+
+// Chap 16 Q08
+// var mStudents = ["Michael", "John", "Tony"];
+// var mMarks = [320, 230, 480];
+
+// document.write("Score of ", mStudents[0], " is ", mMarks[0], ". Percentage: ", ((mMarks[0]*100)/500),"%</br>");
+// document.write("Score of ", mStudents[1], " is ", mMarks[1], ". Percentage: ", ((mMarks[1]*100)/500),"%</br>");
+// document.write("Score of ", mStudents[2], " is ", mMarks[2], ". Percentage: ", ((mMarks[2]*100)/500),"%");
+
+// Chap 16 Q09
+// var mColor = [""];
+// var mColor_01 = "";
+// var mColor_02 = "";
+
+// mColor[0] = prompt("Enter first color");
+// for (var i=0; i<=mColor.length; i++)
+// {
+//     if (mColor[i]!=undefined)
+//     {
+//         document.write(mColor[i] + "</br>");
+//     }
+// }
+
+// mColor[1] = prompt("Enter last color");
+// for (var i=0; i<=mColor.length; i++)
+// {
+//     if (mColor[i]!=undefined)
+//     {
+//         document.write(mColor[i] + "</br>");
+//     }
+// }
+
+// mColor_01 = prompt("Enter 1 of 2 color");
+// mColor_02 = prompt("Enter 2 of 2 color");
+// mColor.unshift(mColor_01, mColor_02);
+// for (var i=0; i<=mColor.length; i++)
+//     {
+//         if (mColor[i]!=undefined)
+//         {
+//             document.write(mColor[i] + "</br>");
+//         }
+//     }
+
+// Chap 16 Q10
+// var mMarks = [320,230,480,120];
+// mMarks = mMarks.sort();
+// for (var i=0; i<=mMarks.length; i++)
+//     {
+//         if (mMarks[i]!=undefined)
+//         {
+//             document.write(mMarks[i] + "</br>");
+//         }
+//     }
+
+// Chap 16 Q11
+//var mcity = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+
+// Chap 16 Q12
+var arr = ["This", "is", "my", "cat"];
+document.write(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3]);
+
